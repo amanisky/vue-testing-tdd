aed04e231d8e4c4ed390c107ae31ded97c3bca91
amanisky <80311364@qq.com>
2022-06-09 10:11:32 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    redirect: '/todos'
   },
   {
     path: '/todos/:filter?',
